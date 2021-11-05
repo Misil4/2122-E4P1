9323a0fe40b7b598117b03fc754894a1e909934f
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2021-11-05 09:24:29 +0100

    Installation and configuration of the QR reader library, it works by opening websites

--- qr/App.js
+++ qr/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 
 import {
@@ -17,42 +9,14 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import ScanScreen from './components/ScanScreen';
 
 const App = () =>{
 
   return (
-    <Text></Text>
+    <ScanScreen></ScanScreen>
   );
 
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
