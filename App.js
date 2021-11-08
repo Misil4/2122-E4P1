b86bb7e3f7ba9599975b3571b77bb631be0e8b7c
Misil4 <carlossilvamarques.cs@gmail.com>
2021-11-08 23:29:26 +0100

    (UPDATE) QR imports and Stack screen links updated

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/home/index'
-import HomeQR from './src/home/qr'
+import HomeQrReader from './src/home/qrReader'
+import HomeQrGenerator from './src/home/qrGenerator'
 import authentification from "./src/home/authentification";
 
 const HomeStack = createNativeStackNavigator();
@@ -15,7 +16,8 @@ export default class App extends Component{
       <NavigationContainer>
         <HomeStack.Navigator>
           <HomeStack.Screen name="Pantalla De Inicio" component={HomeScreen} />
-          <HomeStack.Screen name="QR" component={HomeQR} />
+          <HomeStack.Screen name="QrReader" component={HomeQrReader} />
+          <HomeStack.Screen name="QrGenerator" component={HomeQrGenerator} />
           <HomeStack.Screen name="Google" component={authentification} />
         </HomeStack.Navigator>
       </NavigationContainer>
