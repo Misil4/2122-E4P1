8620f62c59a96baec01aec5dcbfbce5179442451
ander6 <anderunaidel@gmail.com>
2021-11-12 12:32:54 +0100

    Geolocation added

--- App.js
+++ App.js
@@ -8,14 +8,11 @@ import QrReader from "./src/home/qrReader";
 import QrGenerator from "./src/home/qrGenerator";
 import Authentification from "./src/home/authentification";
 import SplashScreen from 'react-native-splash-screen'
-<<<<<<< HEAD
 import Settings from "./src/home/Settings";
 import GarbageLocation from "./src/home/GarbageLocation";
+import geolocation from "./src/home/geolocation";
 
 const drawer = createDrawerNavigator();
-=======
-import geolocation from "./src/home/geolocation";
->>>>>>> Geolocalization
 
 
 export default class App extends Component{
@@ -28,7 +25,6 @@ export default class App extends Component{
     return(
 
       <NavigationContainer>
-<<<<<<< HEAD
         <drawer.Navigator>
           <drawer.Screen name="Log Out" component={Authentification} options={{ headerShown: false , swipeEnabled: false}}/>
           <drawer.Screen name="Inicio" component={HomeScreen} />
@@ -36,16 +32,8 @@ export default class App extends Component{
           <drawer.Screen name="QrGenerator" component={QrGenerator} />
           <drawer.Screen name="Settings" component={Settings} />
           <drawer.Screen name="Garbage" component={GarbageLocation} />
+          <drawer.Screen name="Geolocalization" component={geolocation}/>
         </drawer.Navigator>
-=======
-        <HomeStack.Navigator>
-          <HomeStack.Screen name="Log In" component={Authentification} />
-          <HomeStack.Screen name="PantallaDeInicio" component={HomeScreen} />
-          <HomeStack.Screen name="QrReader" component={QrReader} />
-          <HomeStack.Screen name="QrGenerator" component={QrGenerator} />
-          <HomeStack.Screen name="Geo" component={geolocation} />
-        </HomeStack.Navigator>
->>>>>>> Geolocalization
       </NavigationContainer>
     )
   }
