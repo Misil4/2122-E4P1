8be54fe1805be9164738ed28d00d1daf2bbb72c0
ander6 <anderunaidel@gmail.com>
2021-11-12 10:25:52 +0100

    (UPDATE) Navigation menu change to drawer

--- App.js
+++ App.js
@@ -3,13 +3,17 @@
 import React, { Component } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/home/index'
 import QrReader from "./src/home/qrReader";
 import QrGenerator from "./src/home/qrGenerator";
 import Authentification from "./src/home/authentification";
 import SplashScreen from 'react-native-splash-screen'
 
-const HomeStack = createNativeStackNavigator();
+const stack = createNativeStackNavigator();
+const drawer = createDrawerNavigator();
+const tab = createBottomTabNavigator();
 
 export default class App extends Component{
 
@@ -21,12 +25,12 @@ export default class App extends Component{
     return(
 
       <NavigationContainer>
-        <HomeStack.Navigator>
-          <HomeStack.Screen name="Log In" component={Authentification} />
-          <HomeStack.Screen name="PantallaDeInicio" component={HomeScreen} />
-          <HomeStack.Screen name="QrReader" component={QrReader} />
-          <HomeStack.Screen name="QrGenerator" component={QrGenerator} />
-        </HomeStack.Navigator>
+        <drawer.Navigator>
+          <drawer.Screen name="Log In" component={Authentification} options={{ headerShown: false , swipeEnabled: false}}/>
+          <drawer.Screen name="PantallaDeInicio" component={HomeScreen} />
+          <drawer.Screen name="QrReader" component={QrReader} />
+          <drawer.Screen name="QrGenerator" component={QrGenerator} />
+        </drawer.Navigator>
       </NavigationContainer>
     )
   }
